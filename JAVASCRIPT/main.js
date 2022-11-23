528872b0369a5830527b5927b44e08bc30e5678c
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2022-11-23 14:51:47 +0530

    Add files via upload

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,77 @@
+// function first(){
+//     console.log('first')
+//     second();
+// }
+// function second(){
+//     console.log('second')
+//     third();
+// }
+// function third(){
+//     console.log('third')
+// }
+// first();
+
+// let num = 10;
+// console.log(num);
+// let user = 20;
+// let name ='john';
+// let age = 24;
+// console.log(user);
+// console.log(name);
+// console.log(age);
+
+// let age = prompt('what is your age',18); //prompt
+// alert(`your age is ${age}`) //alert
+
+// let a = prompt('Enter a: ');
+// let b = prompt('Enter b: ');
+// var isStudent = 4>2;
+// confirm(isStudent);
+
+// function myAge(a,b){
+//     let sum = a+b;
+//     // console.log("my age is 18");
+//     // console.log(a+b)
+//     return sum;
+// }
+// console.log(myAge(50,10));
+
+// Arrow Functions
+// let myAge = ()=>{
+//     console.log("18");
+// }
+// myAge();
+// let myAge = (a,b)=> a+b;
+// console.log(myAge(10,8));
+
+// Strings
+// let str = "this is a string";
+// let num = 25;
+// console.log(str,typeof str);
+// console.log(str[1]);
+// console.log(str.length);
+// console.log(str.slice(1,3));
+// console.log(str.substring(0,10))
+// for (let index = 0; index < str.length; index++) {
+//     var a = str[index];
+//     console.log(a);
+// }
+
+// var sum = 0;
+// for(; ;){
+//     let a = prompt();
+//     sum = +sum + +a;
+//     if(a==null){
+//         console.log(sum);
+//         break;
+//     }
+// }
+
+// for(let i = 1; i<10;i++){
+//     if(i%2==0){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
